ce268724aee3583cb4f3f66ddcfc4d6279e8c66b
Pol Olvd <pauline.olliveaud@gmail.com>
2020-02-10 22:31:14 +0100

    commit addOuting form

--- src/Controller/OutingController.php
+++ src/Controller/OutingController.php
@@ -2,18 +2,43 @@
 
 namespace App\Controller;
 
+use App\Entity\Outing;
+use App\Form\OutingType;
+use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\Routing\Annotation\Route;
 
 class OutingController extends Controller
 {
     /**
-     * @Route("/outing", name="outing")
+     * @Route("/add", name="add_outing")
      */
-    public function index()
+    public function addOuting(EntityManagerInterface $entityManager, Request $request)
     {
-        return $this->render('outing/index.html.twig', [
-            'controller_name' => 'OutingController',
-        ]);
+        $outing = new Outing();
+        // Todo : ajout de l'user pour chaque sortie
+
+        $outingForm = $this->createForm(OutingType::class, $outing);
+        $outingForm->handleRequest($request);
+
+        if($outingForm->isSubmitted()){
+
+            $this->addFlash(
+                'success',
+                'Sortie ajoutée avec succès'
+            );
+
+            $entityManager->persist($outing);
+            $entityManager->flush();
+
+            return $this->redirectToRoute("home");
+        }
+
+        return $this->render('outing/add.html.twig',
+            [
+            'outingFormView' => $outingForm->createView(),
+            ]
+        );
     }
 }
--- src/Form/OutingType.php
+++ src/Form/OutingType.php
@@ -4,6 +4,9 @@ namespace App\Form;
 
 use App\Entity\Outing;
 use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\Extension\Core\Type\DateTimeType;
+use Symfony\Component\Form\Extension\Core\Type\DateType;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
 
@@ -12,19 +15,51 @@ class OutingType extends AbstractType
     public function buildForm(FormBuilderInterface $builder, array $options)
     {
         $builder
-            ->add('name')
-            ->add('startDate')
-            ->add('duration')
-            ->add('limitDateSub')
-            ->add('closingDate')
-            ->add('numberMaxSub')
-            ->add('numberSub')
-            ->add('description')
-            ->add('cancelInfo')
-            ->add('image')
-            ->add('site')
-            ->add('state')
-            ->add('place')
+            ->add(
+                'name',
+                TextType::class,
+                [
+                    'label' => 'Nom de la sortie'
+                ]
+            )
+            ->add(
+                'startDate',
+                DateTimeType::class,
+                [
+                    'label' => 'Date et heure de la sortie'
+                 ]
+            )
+            ->add(
+                'limitDateSub',
+                DateType::class,
+                [
+                    'label' => 'Date limite d\'inscription'
+                ]
+            )
+            ->add(
+                'numberMaxSub',
+                TextType::class,
+                [
+                    'label' => 'Nombre de places'
+                ]
+            )
+            ->add(
+                'duration',
+                TextType::class,
+                [
+                    'label' => 'Durée'
+                ]
+            )
+            ->add(
+                'description',
+                TextType::class,
+                [
+                    'label' => 'Description et infos'
+                ]
+            )
+            //->add('site')
+            //->add('state')
+            //->add('place')
         ;
     }
 
