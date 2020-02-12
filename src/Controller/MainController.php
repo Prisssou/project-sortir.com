25280fa49dddd666333736db32ee81fb334b21fd
Alan <alan.propacaud@gmail.com>
2020-02-12 09:06:28 +0100

    filterform in progress

--- src/Controller/MainController.php
+++ src/Controller/MainController.php
@@ -2,16 +2,42 @@
 
 namespace App\Controller;
 
+use App\Form\FilterFormType;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\Security\Core\Encoder\UserPasswordEncoderInterface;
 
 class MainController extends Controller
 {
     /**
      * @Route("/", name="home")
      */
-    public function home()
+    public function home(Request $request, UserPasswordEncoderInterface $passwordEncoder): Response
     {
+
+
+
+        $filterForm = $this->createForm(FilterFormType::class);
+        $filterForm->handleRequest($request);
+
+        if ($filterForm->isSubmitted() && $filterForm->isValid()) {
+
+
+
+            return $this->redirectToRoute("user_login");
+
+        }
+
+
+        return $this->render(
+            'main/home.html.twig',
+            [
+                'MemberFormView' => $filterForm->createView(),
+            ]
+        );
+
         return $this->render('main/home.html.twig');
     }
 }
