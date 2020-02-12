41afb98870e13b328ff434cacc40e7bcdabd7844
Alan <alan.propacaud@gmail.com>
2020-02-12 11:58:34 +0100

    Outing table display & adding sql fixture

--- src/Controller/MainController.php
+++ src/Controller/MainController.php
@@ -2,42 +2,46 @@
 
 namespace App\Controller;
 
+
+use App\Entity\Outing;
 use App\Form\FilterFormType;
+use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
-use Symfony\Component\HttpFoundation\Request;
-use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
-use Symfony\Component\Security\Core\Encoder\UserPasswordEncoderInterface;
 
 class MainController extends Controller
 {
     /**
      * @Route("/", name="home")
      */
-    public function home(Request $request, UserPasswordEncoderInterface $passwordEncoder): Response
+    public function home(EntityManagerInterface $entityManager)
     {
+        $sortieRepository = $entityManager->getRepository(Outing::class);
+        $sorties = $sortieRepository->findAll();
 
 
 
-        $filterForm = $this->createForm(FilterFormType::class);
-        $filterForm->handleRequest($request);
-
-        if ($filterForm->isSubmitted() && $filterForm->isValid()) {
 
 
+//        $filterForm = $this->createForm(FilterFormType::class);
+//        $filterForm->handleRequest($request);
+//
+//        if ($filterForm->isSubmitted() && $filterForm->isValid()) {
+//
+//
+//
+//            return $this->redirectToRoute("user_login");
+//
+//        }
+//
+//
+//        return $this->render(
+//            'main/home.html.twig',
+//            [
+//                'MemberFormView' => $filterForm->createView(),
+//            ]
+//        );
 
-            return $this->redirectToRoute("user_login");
-
-        }
-
-
-        return $this->render(
-            'main/home.html.twig',
-            [
-                'MemberFormView' => $filterForm->createView(),
-            ]
-        );
-
-        return $this->render('main/home.html.twig');
+        return $this->render('main/home.html.twig', compact('sorties'));
     }
 }
