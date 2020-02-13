62d15de8152be79095a474ebe24801e224119121
Pauline <pauline.olliveaud@gmail.com>
2020-02-13 11:30:23 +0100

    commit form outing

---
+++ src/Entity/Ville.php
@@ -0,0 +1,108 @@
+<?php
+
+namespace App\Entity;
+
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * @ORM\Entity(repositoryClass="App\Repository\VilleRepository")
+ */
+class Ville
+{
+    /**
+     * @ORM\Id()
+     * @ORM\GeneratedValue()
+     * @ORM\Column(type="integer")
+     */
+    private $id;
+
+    /**
+     * @ORM\Column(type="string", length=255)
+     */
+    private $nom;
+
+    /**
+     * @ORM\Column(type="string", length=6)
+     */
+    private $code;
+
+    /**
+     * @ORM\Column(type="string", length=255)
+     */
+    private $codesPostaux;
+
+    /**
+     * @ORM\Column(type="integer")
+     */
+    private $codeDepartement;
+
+    /**
+     * @ORM\Column(type="string", length=255)
+     */
+    private $departement;
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getNom(): ?string
+    {
+        return $this->nom;
+    }
+
+    public function setNom(string $nom): self
+    {
+        $this->nom = $nom;
+
+        return $this;
+    }
+
+    public function getCode(): ?string
+    {
+        return $this->code;
+    }
+
+    public function setCode(string $code): self
+    {
+        $this->code = $code;
+
+        return $this;
+    }
+
+    public function getCodesPostaux(): ?string
+    {
+        return $this->codesPostaux;
+    }
+
+    public function setCodesPostaux(string $codesPostaux): self
+    {
+        $this->codesPostaux = $codesPostaux;
+
+        return $this;
+    }
+
+    public function getCodeDepartement(): ?int
+    {
+        return $this->codeDepartement;
+    }
+
+    public function setCodeDepartement(int $codeDepartement): self
+    {
+        $this->codeDepartement = $codeDepartement;
+
+        return $this;
+    }
+
+    public function getDepartement(): ?string
+    {
+        return $this->departement;
+    }
+
+    public function setDepartement(string $departement): self
+    {
+        $this->departement = $departement;
+
+        return $this;
+    }
+}
