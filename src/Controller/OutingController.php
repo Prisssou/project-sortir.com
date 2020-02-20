e12fb9c1cdb47efaffde8abf448ee11efb85e367
Prisssou <priscilla.anciaux@gmail.com>
2020-02-20 15:59:36 +0100

    design

--- src/Controller/OutingController.php
+++ src/Controller/OutingController.php
@@ -114,7 +114,6 @@ class OutingController extends Controller
 
         // Attributs pour conditions
         $numSubs = $outing->getSubscriptions();
-        dump($numSubs);
         $maxSubs = $outing->getNumberMaxSub();
         $dateMaxSub = $outing->getLimitDateSub();
         $today = date('now');
